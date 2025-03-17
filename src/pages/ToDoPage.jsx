d66259748fbf8bde78b2cbe67c7d6313b7142d55
Houy-G <la9089975@gmail.com>
2025-03-17 09:20:22 +0700

    Houy

--- src/pages/ToDoPage.jsx
+++ src/pages/ToDoPage.jsx
@@ -13,7 +13,6 @@ export default function ToDoPage() {
                     <div onClick={() => {setProgress(!progress)}}>
                         <TodoCardList/>
                     </div>
-
                     <div className={"lg:w-[780px] w-96 lg:mt-0 md:mt-16 overscroll-none hidden md:block md:h-[600px] lg:h-[500px] 2xl:h-auto bg-gray-50 dark:bg-gray-700  overflow-hidden rounded-2xl overflow-y-scroll scroll-smooth "}>
                         {progress &&(<TodoCardDetail/>)}
                     </div>
